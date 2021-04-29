2282dcf9c4e40c5af3e1b5aae6099a53587f1090
Lein Matsumaru <appleprincess@appleprincess.io>
2021-04-29 17:10:15 +0000

    Add packages

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='sherlock',
     version='0.14.0',
+    packages = ["sherlock"],
     entry_points={
         'console_scripts': [
-            'sherlock=sherlock:main'
+            'sherlock=sherlock.sherlock:main'
         ]
     }
 )
